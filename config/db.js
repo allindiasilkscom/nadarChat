79c93dd7b107c4adb37b01e587ca135a68209ece
allindiasilkscom <153381149+allindiasilkscom@users.noreply.github.com>
2023-12-28 15:04:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,14 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB".green); // Assuming 'green' is a colors.js method
   } catch (error) {
-    console.log(`error in DB ${error}`.america.bgGreen);
+    console.error(`Error in DB: ${error.message}`.bgGreen.red); // Formatting the error message
   }
 };
 
-
-module.exports = connectDB
+module.exports = connectDB;
